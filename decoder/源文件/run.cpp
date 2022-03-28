540564e9586cfd8624e2dc039dd8ffab34ae487b
xingjian zhou <815995571@qq.com>
2022-03-29 02:13:00 +0800

    decoder更新

--- decoder/源文件/run.cpp
+++ decoder/源文件/run.cpp
@@ -8,96 +8,117 @@
 #include<fstream>
 #include <cassert>
 #include<json/json.h>
+#include "../../encoder/encoder.h"
 using namespace cv;
 using namespace std;
 
 
 int main(int argc, char** argv) {
-	if (argc != 3) {
-		cout << "请输入两个参数，第一个为图片路径，第二个为存放信息的json文件" << endl;
+	if (argc != 2) {
+		cout << "请输入图片路径" << endl;
 		return -1;
 	}
 
 	Mat image;
 	image = imread(argv[1], IMREAD_GRAYSCALE); // Read the file
 
-	string addInfo = argv[2];
-	Json::Reader reader;
-	Json::Value root;
-	ifstream ifs;
-	ifs.open(addInfo);
-	if (!reader.parse(ifs, root, false))
+	additonalInfo addinfo;
+	pair<int, int> firstpair = make_pair(image.at<uchar>(0, 0), image.at<uchar>(0, 1));
+	image.at<uchar>(0, 0) = 0; image.at<uchar>(0, 1) = 0;
+	if (image.empty())
 	{
-		cout << "reader parse error: " << strerror(errno) << endl;
+		cout << "Could not open or find the image" << endl;
 		return -1;
-	}
-
-	int length = root["length"].asInt();
-	vector<int> mask;
-	for (int i = 0; i < 256; i++) {
-		mask.push_back(root["mask"][i].asInt());
-	}
-
-	
-	for (int flag = 0, index = 0, i = 0; i < 256; i++) {
-		if (mask[i] == 3) { flag = 1; index = i;}
-		else if (mask[i] == -2) { flag = -1;  index = i;}
-
-		if (flag == 0) {continue;}
-		else if (flag == 1) {
-			if (mask[i] == 2) {
-				mask[index+1] = 2;
-				mask[i] = 1;
-				flag = 0;
-			}
-		}
-		else if (flag == -1) {
-			if (mask[i] == -3) {
-				mask[index] = -2;
-				mask[i-1] = -1;
-				flag = 0;
-			}
-		}
 	}
 
 
 	string embedInfo;
-	for (int i = 0; i < image.rows; i++) {
-		for (int j = 0; j < image.cols; j++) {
-			if (mask[image.at<uchar>(i, j)] == 0) {
-				continue;
-			}
-			else if (mask[image.at<uchar>(i, j)] == 1) {
-				image.at<uchar>(i, j)--;
-			}
-			else if (mask[image.at<uchar>(i, j)] == 2) {
-				embedInfo += "1";
-				image.at<uchar>(i, j)--;
-			}
-			else if (mask[image.at<uchar>(i, j)] == 3) {
-				embedInfo += "0";
-			}
-			else if (mask[image.at<uchar>(i, j)] == -1) {
-				image.at<uchar>(i, j)++;
-			}
-			else if (mask[image.at<uchar>(i, j)] == -2) {
-				embedInfo += "1";
-				image.at<uchar>(i, j)++;
-			}
-			else if (mask[image.at<uchar>(i, j)] == -3) {
-				embedInfo += "0";
-			}
+	
 
-			if (embedInfo.length() == length) {
-				goto here;
+	if (firstpair.first > firstpair.second) {
+
+		for (int j = 2; j < image.cols; j++) {
+			if (image.at<uchar>(0, j) == firstpair.first) {
+				embedInfo += '0';
+			}
+			else if (image.at<uchar>(0, j) == firstpair.first - 1) {
+				embedInfo += '1';
 			}
 		}
+		for (int i = 1; i < image.rows; i++) {
+			for (int j = 0; j < image.cols; j++) {
+				if (image.at<uchar>(i, j) == firstpair.first) {
+					embedInfo += '0';
+				}
+				else if (image.at<uchar>(i, j) == firstpair.first - 1) {
+					embedInfo += '1';
+				}
+			}
+		}
+
+	}
+	else {
+
+		for (int j = 2; j < image.cols; j++) {
+			if (image.at<uchar>(0, j) == firstpair.first) {
+				embedInfo += '0';
+			}
+			else if (image.at<uchar>(0, j) == firstpair.first + 1) {
+				embedInfo += '1';
+			}
+		}
+		for (int i = 1; i < image.rows; i++) {
+			for (int j = 0; j < image.cols; j++) {
+				if (image.at<uchar>(i, j) == firstpair.first) {
+					embedInfo += '0';
+				}
+				else if (image.at<uchar>(i, j) == firstpair.first + 1) {
+					embedInfo += '1';
+				}
+			}
+		}
+
+	}
+
+
+	if (embedInfo.size() < 72) {
+		cout << "这个算法不应该出现这样的结果，请在github的issue中提交你的用例";
+		return -1;
+	}
+	string totalAddLength = embedInfo.substr(0, 16);
+	string toembedLength = embedInfo.substr(32, 16);
+	for (int i = 15,mask = 1; i >= 0;i--) {
+		if (totalAddLength[i] == '1') {
+			addinfo.totalAddLength += mask;
+		}
+		if (toembedLength[i] == '1') {
+			addinfo.toembedLength += mask;
+		}
+		mask <<= 1;
+	}
+
+	string imageGrayscaleAt_0_0 = embedInfo.substr(16, 8);
+	string imageGrayscaleAt_0_1 = embedInfo.substr(24, 8);
+	string lengthForBitmapMinLength = embedInfo.substr(48,8);
+	for (int i = 8, mask = 1; i >= 0; i--) {
+		if (imageGrayscaleAt_0_0[i] == '1') {
+			addinfo.imageGrayscaleAt_0_0 += mask;
+		}
+		if (lengthForBitmapMinLength[i] == '1') {
+			addinfo.lengthForBitmapMinLength += mask;
+		}
+		mask <<= 1;
+	}
+
+	pair<int, int> thispair = firstpair;
+	while (1) {
+		
 	}
 
 
 here:
 	imwrite("output.bmp", image);
-	cout << embedInfo << endl;
+
 
 	return 0;
 }
--- encoder/encoder.h
+++ encoder/encoder.h
@@ -4,6 +4,7 @@
 using namespace std;
 class additonalInfo {
 public:
+	additonalInfo(){}
 	additonalInfo(int length, int imageGrayscale_0_0, int imageGrayscale_0_1) {
 		for (int i = 0; i < 256; i++) {
 			this->mask.push_back(0);
@@ -13,18 +14,19 @@ public:
 		imageGrayscaleAt_0_1 = imageGrayscale_0_1;
 	};
 
-	int imageGrayscaleAt_0_0, imageGrayscaleAt_0_1;  //固定16bits
-
 	vector<int> mask;  //已废弃
 
+	int imageGrayscaleAt_0_0 = -1, imageGrayscaleAt_0_1 = -1;  //固定16bits
 	vector<pair<int, int>> pairs;  //每对8+8bits
-	int toembedLength;  //  固定16bits
+	int toembedLength = -1;  //  固定16bits
 	vector<pair<int,int>> bitmap1;   //每个位置16bits
 	vector<int> bitmapMinLength;     //最多128对，即最多128个非零零值点，每个零值点的最大值为107，所以不能为这个数组分配固定长度
 	int lengthForBitmapMinLength = 0;    //上述数组的长度，也即非零零值点的个数，固定8bits
 	//vector<pair<unsigned short, unsigned short>> bitmap2;
 
-	//这个函数在转换成员变量时会将新变量添加到字符前，因此decoder端先读到内容在这个函数里写在最后
+	int totalAddLength = -1;
+
+	//这个函数在转换成员变量时会将新变量添加到字符前，因此decoder端先读到的内容在这个函数里写在最后
 	string AddInfo() {
 		string extendInfo;
 
@@ -63,19 +65,19 @@ public:
 			}
 		}
 
-		//转换lengthForBitmapMinLength
+		//转换lengthForBitmapMinLength(8bits)
 		for (int i = 0, lengthForBitmapMinLength = bitmapMinLength.size(); i < 8; i++) {
 			extendInfo = to_string(lengthForBitmapMinLength & 1) + extendInfo;
 			lengthForBitmapMinLength >>= 1;
 		}
 
-		//转换toembedLength
+		//转换toembedLength(16bits)
 		for (int toEmbedLength = toembedLength, i = 0; i < 16; i++) {
 			extendInfo = to_string(toEmbedLength & 1) + extendInfo;
 			toEmbedLength >>= 1;
 		}
 
-		//转换imageGrayscaleAt_0_0,imageGrayscaleAt_0_1
+		//转换imageGrayscaleAt_0_0,imageGrayscaleAt_0_1(8+8bits)
 		int temp = imageGrayscaleAt_0_0;
 		for (int i = 0; i < 8; i++) {
 			extendInfo = to_string(temp & 1) + extendInfo;
@@ -84,6 +86,14 @@ public:
 
 		temp = imageGrayscaleAt_0_1;
 		for (int i = 0; i < 8; i++) {
+			extendInfo = to_string(temp & 1) + extendInfo;
+			temp >>= 1;
+		}
+
+		//添加整体addinfo的长度(16bits)，包含自身
+		totalAddLength = extendInfo.length() + 16;
+		temp = totalAddLength;
+		for (int i = 0; i < 16; i++) {
 			extendInfo = to_string(temp & 1) + extendInfo;
 			temp >>= 1;
 		}
